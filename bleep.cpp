3d9852002a95a46422f33504339313db17d47dbf
beniandthe <125909278+beniandthe@users.noreply.github.com>
2024-01-30 13:04:19 -0800

    Everything there but stuck on replacing the bleep word

--- bleep.cpp
+++ bleep.cpp
@@ -19,37 +19,50 @@ void introduction () {
 }
 
 void bleep(string &word, string &sentence){
-    // pass by reference word and sentence
-    string bleeped_word = word;
-    string bleeped_sentence = sentence;
-    int word_size = word.size();
-   
-    // loop through sentence string. 
-    for (int i = 0; i < sentence.size(); i++) 
+  // pass by reference word and sentence
+  string bleeped_word = word;
+  string bleeped_sentence = sentence;
+  int word_size = word.size();
+  
+  // loop through sentence string. 
+  for (int i = 0; i < sentence.size(); i++) 
+  {
+    int match_count = 0;
+    cout << "Sentence Index: " << i << "\n";
+        
+    //loop through word string
+    for (int j = 0; j < word.size(); j++) 
     {
-      int match_count = 0;
-      cout << "Sentence Index: " << i << "\n";
-        //loop through word string
-        for (int j = 0; j < word.size(); j++) 
-        {
-          cout << "Word index: " << j << "\n";
-            // check if sentence at index plus character j is equal to word at character j
-            if (sentence[i + j] == word[j])
-            {
-              cout << "Match Found\n";
-              match_count += 1;
-              cout << "Match count: " << match_count << "\n";
-            }
-            else
-            {
-              break;
-            }
-          if (match_count == word_size)
-          {
-            bleeped_sentence[i + j] = '*';
-          }
+      cout << "Word index: " << j << "\n";
+            
+      // check if sentence at index plus index j is equal to word at character j
+      if (sentence[i + j] == word[j])
+      {
+        cout << "Match Found\n";
+        match_count += 1;
+        cout << "Match count: " << match_count << "\n";
+
+        // check if match count equals the size of the word. if yes, bleep sentence at i + j.
+        if (match_count == word_size)
+        { 
+          match == true;
+          bleeped_sentence[i] = '*';
         }
+
+        else
+        {
+          match = false;
+        }
+      }
+        
+      // if not, break the loop
+      else
+      {
+        break;
+      }
+
     }
+  }
   cout << "\n" << bleeped_sentence;
   return;
 }
