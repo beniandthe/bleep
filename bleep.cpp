64c89d6ddc074ac92d1abfc015bc30a6bf6d6212
beniandthe <125909278+beniandthe@users.noreply.github.com>
2024-01-30 14:21:15 -0800

    Adding statements to intro to turn off index showing

--- bleep.cpp
+++ bleep.cpp
@@ -5,15 +5,38 @@ using namespace std;
 string sentence;
 string word;
 string word_in_sentence;
+string yes_or_no;
 
-
+bool skip_it = false;
 
 void introduction () {
   cout << "Let's bleep the **** out of some words!\n";
-  cout << "Please type a sentence/paragraph/monologue, whatever the **** you want: \n";
+  cout << "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n";
+  cout << "\nPlease type a sentence/paragraph/monologue, whatever the **** you want: \n";
   getline(cin, sentence);
   cout << "\nNow type the word you would like to bleep: \n";
   cin >> word;
+
+  cout << "\nLastly, would you like to turn on visual indexing algorithm? Y/N: \n";
+  cin >> yes_or_no;
+  
+  while (yes_or_no != "Y" || yes_or_no != "N")
+  {
+    cout << "Please enter Y or N: ";
+    cin >> yes_or_no;
+  
+    if (yes_or_no == "Y")
+    {
+      skip_it = false;
+    } 
+
+    else if (yes_or_no == "N")
+    {
+      skip_it = true;
+    }
+  }
+  
+  cout << "\nBleeping..." << word;
   cout << "\n~~~~~~~~~~~~~~\n";
 }
 
@@ -27,13 +50,14 @@ void bleep(string &word, string &sentence){
   for (int i = 0; i < sentence.size(); i++) 
   {
     int match_count = 0;
+    
     cout << "Sentence Index: " << i << "\n";
-        
+
     //loop through word string
     for (int j = 0; j < word.size(); j++) 
     {
       cout << "Word index: " << j << "\n";
-            
+ 
       // check if sentence at index plus index j is equal to word at character j
       if (sentence[i + j] == word[j])
       {
@@ -67,6 +91,7 @@ void bleep(string &word, string &sentence){
 
 
 int main() {
+  
   introduction();
 
   bleep(word, sentence);
