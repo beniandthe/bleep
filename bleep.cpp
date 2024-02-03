9ba00e04c83e1a852c037c7e8c54a87e004dad4c
beniandthe <125909278+beniandthe@users.noreply.github.com>
2024-02-03 10:53:09 -0800

    deleted unnecessary else clauses and updating is in word func

--- bleep.cpp
+++ bleep.cpp
@@ -12,6 +12,7 @@ bool dont_skip_it = true;
 string bleeped_word = word;
 string bleeped_sentence = sentence;
 int word_size = word.size();
+int match_count;
 
 
 void introduction () {
@@ -22,7 +23,7 @@ void introduction () {
   cout << "\nNow type the word you would like to bleep: \n";
   cin >> word;
 
-  cout << "\nLastly, would you like to turn on visual indexing algorithm? Y/N: \n";
+  cout << "\nLastly, would you like to turn on visual indexing? Y/N: \n";
   cin >> yes_or_no;
   
 
@@ -49,26 +50,59 @@ void introduction () {
 bool word_in_word(){
 
 bool is_in_word = true;
-
+bool space_in_front = false;
+bool space_in_back = false;
   for (int i = 0; i < sentence.size(); i++)
   {
     for (int j = 0; j < word.size(); j++)
-    {
-      if (isspace(bleeped_sentence[i - 1]) && isspace(bleeped_sentence[i + word_size + 1]))
+    { 
+      if (match_count == 1)
       {
-        is_in_word = false;
-        return is_in_word;
+        if (isspace(sentence[i - 1]))
+        {
+          space_in_front = true;
+          return space_in_front;
+        }
+        else
+        {
+          space_in_front = false;
+          return space_in_front;
+        }
       }
+
+      else if (match_count == word_size)
+      {
+        if (isspace(sentence[i + word_size]))
+        {
+          space_in_back = true;
+          return space_in_back;
+        }
+        else
+        {
+          space_in_back = false;
+          return space_in_back;
+        }
+      }
+
       else
       {
-        return is_in_word = true;
+        break;
       }
     }
   }
-  return is_in_word;
-}
 
+  if (space_in_front == true && space_in_back == true)
+  {
+    is_in_word = false;
+    return is_in_word;
+  }
 
+  else
+  {
+    is_in_word = true;
+    return is_in_word;
+  }
+} 
 
 
 void bleep(string &word, string &sentence){
@@ -81,41 +115,33 @@ void bleep(string &word, string &sentence){
   for (int i = 0; i < sentence.size(); i++) 
   {
     int match_count = 0;
+    // visual indexing
     if (yes_or_no == "Y")
     {
     cout << "Sentence Index: " << i << "\n";
     }
-    else
-    {
-      false;
-    }
+
     //loop through word string
     for (int j = 0; j < word.size(); j++) 
     {
+      // visual indexing
       if (yes_or_no == "Y")
       {
       cout << "Word index: " << j << "\n";
       }
-      else
-      {
-        false;
-      }
+
       // check if sentence at index plus index j is equal to word at character j
       if (sentence[i + j] == word[j])
       {
         match_count += 1;
+        word_in_word();
         if (yes_or_no == "Y")
         {         
         cout << "Match Found\n";
-        cout << "Match count: " << match_count << "\n";
+        cout << "Match count: " << match_count << "\n\n";
         }
-        else
-        {
-          false;
-        }
-
       }
-        
+  
       // if not, break the loop
       else
       {
@@ -126,7 +152,7 @@ void bleep(string &word, string &sentence){
     
     // if match count = word size
     // loop through the word index j and set i + j at sentence to *
-    if (match_count == word_size && !word_in_word())
+    if (match_count == word_size)
     {
       for (int j = 0; j < word_size; j++)
       {
